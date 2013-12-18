1d2698a9a6522b729f87ecf49432ec2f7db23a0e
Humberto Gómez López <hoijof@iyv-corp.com>
2013-12-18 05:44:25 +0100

    Basic pigs movments

---
+++ js/Pig.model.js
@@ -0,0 +1,60 @@
+var Pig = function () {
+    this.img = undefined;
+    this.container = undefined;
+    this.finalX = 0;
+    this.finalY = 0;
+    this.restart = 0;
+    this.animationTime = 1000;
+    this.bounce = false;
+    var self = this;
+
+    this.travel = function () {
+        if (this.restart < 0) setTimeout(function() { self.travel() }, getRandomInt(700,1400));
+        else {
+            //TODO change the algorithm to create new pigs
+            if (this.finalX > canvas.width/2 && pigs.length < gameConfig.maxPigs) pigs[pigs.length] = new Pig().travel();
+            if (this.finalX > canvas.width + gameConfig.pig.width){
+                this.finalX = this.finalY = 0;
+                this.container.x = -gameConfig.pig.width-50;
+                this.restart = -100;
+                this.container.scaleX = this.container.scaleY = getRandomInt(50,150)/100;
+                this.travel();
+            } else {
+                this.finalX += getRandomInt(180,200);
+                if (this.bounce) {
+                    this.finalY -= getRandomInt(20,80);
+                        if (this.finalY - gameConfig.pig.height < 0) this.finalY = gameConfig.pig.height;
+                    this.bounce = false;
+                } else {
+                    this.bounce = true;
+                    this.finalY += getRandomInt(20,80);
+                    if (this.finalY + gameConfig.pig.height > 360) this.finalY = 360-gameConfig.pig.height;
+                }
+                createjs.Tween.get(this.container).to({x: this.finalX, y: this.finalY}, this.animationTime).call(this.travel,[],this);
+            }
+
+        }
+        return this;
+    };
+
+    this.init = function() {
+        this.container = new createjs.Container();
+        this.img = new createjs.Bitmap(queue.getResult('pig1'));
+        var width = gameConfig.pig.width/2;
+        var height = gameConfig.pig.height/2;
+        this.img.set({regX: width,regY: height,x: width + 20,y: height + 20})
+        this.container.addChild(this.img);
+        stage.addChild(this.container);
+        return this;
+    };
+    this.show = function() {
+        this.container.visible = true;
+        return this;
+    };
+    this.hide = function() {
+        this.container.visible = false;
+        return this;
+    };
+    this.init();
+    return this;
+}
--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@
  * Created by IVY-CORP-PC001 on 17/12/13.
  */
 
+var gameConfig = {
+    pig: {
+        width: 86,
+        height: 64
+    },
+    maxPigs: 5
+}
+
+var pigs = [0];
 var stage = undefined;
 var canvas = undefined;
-var circle,circle2;
+var circle,circle2,pig;
+var queue;
 var prueba = undefined;
 
-
-function handleComplete() {
-    createjs.Sound.play("sound");
-    var image = queue.getResult("pig1");
-    document.body.appendChild(image);
-}
 
 function init(){
 
@@ -21,9 +25,9 @@ function init(){
     canvas.height = 768;
     stage = new createjs.Stage(canvas);
 
-    var queue = new createjs.LoadQueue();
+    queue = new createjs.LoadQueue();
     //queue.installPlugin(createjs.Sound);
-    queue.on("complete", handleComplete, this);
+    queue.on("complete", drawExample, this);
     //queue.loadFile({id:"sound", src:"http://path/to/sound.mp3"});
     queue.loadManifest([
         {id: "pig1", src:"img/pig.png"},
@@ -31,34 +35,31 @@ function init(){
         {id: "far",  src:"img/backgroundFarm.png"}
     ]);
 
-
     createjs.Ticker.setFPS(25);
 
 }
 
 function tick(event) {
-    circle.alpha = .2;
-    circle2.rotation += 3;
-
-    var pt = circle2.localToLocal(96, 0, circle);
-    if(circle.hitTest(pt.x, pt.y)) circle.alpha = 1;
     stage.update(event);
+    pigs.forEach(function (pig){
+       pig.restart++;
+    })
+
 }
-function drawExample    () {
-    circle = stage.addChild(new createjs.Shape()) ;
-    circle.graphics.beginFill('red').drawCircle(50, 50, 50);
-    circle.set({x:0, y:0, alpha: 0.2});
+function drawExample() {
 
-    circle2 = stage.addChild(new createjs.Shape()) ;
-    circle2.graphics.beginFill("black").drawRect(-2,-2,100,4)
-           .beginFill("blue").drawCircle(100,0,8);
-
-    circle2.set({x:200, y:250, alpha: 1});
+    pigs[0] = new Pig().travel();
     stage.update(event);
 
+
+
+//    createjs.Tween.get(pig).to({x: canvas.width + gameConfig.pig.width/2, y: gameConfig.pig.height/2 + 20, rotation: 360}, 2000);
     createjs.Ticker.addEventListener('tick',tick);
 }
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function main() {
     init();
-    drawExample();
 }
